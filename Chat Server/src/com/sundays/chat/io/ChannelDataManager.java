bb5340d6726573b188a46d31936bb17edbfb170c
FrancisG-Massey <francisg.massey@gmail.com>
2015-08-28 22:31:20 +1200

    Updated Copyright

--- Chat Server/src/com/sundays/chat/io/ChannelDataManager.java
+++ Chat Server/src/com/sundays/chat/io/ChannelDataManager.java
@@ -1,21 +1,21 @@
 /*******************************************************************************
- * Copyright (c) 2013 Francis G.
- * 
- * This file is part of ChatServer.
- * 
- * ChatServer is free software: you can redistribute it and/or modify
+ * Copyright (c) 2015 Francis G.
+ *
+ * This file is part of ${enclosing_project}.
+ *
+ * ${enclosing_project} is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * ChatServer is distributed in the hope that it will be useful,
+ *
+ * ${enclosing_project} is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with ChatServer.  If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+ *******************************************************************************/
 package com.sundays.chat.io;
 
 import java.util.List;
