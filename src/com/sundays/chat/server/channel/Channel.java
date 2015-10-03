dd41e759ebb09597ee22192172970649da224b6d
Francis <francisg.massey@gmail.com>
2015-10-03 14:13:13 +1300

    Added JUnit tests to build script.

--- src/com/sundays/chat/server/channel/Channel.java
+++ src/com/sundays/chat/server/channel/Channel.java
@@ -29,7 +29,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.apache.log4j.Logger;
 
